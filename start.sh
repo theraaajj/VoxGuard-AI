8ae0b66a685e76e96c136e42c166b4568e6d893d
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2026-02-18 19:54:47 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. Start the Background Monitor (Silently)
-# The '&' sends it to the background so the script continues
-python -m components.monitor &
-
-# 2. Start the Streamlit Dashboard
-# Render provides the PORT variable automatically
-streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0
